eed8716fbeadaade5cbf9ea11029f38dc581c8dd
a <ruan65@gmail.com>
2019-02-17 13:59:59 +0300

    refactoring

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 
-import 'package:af_weather/bloc/weather/weather_events.dart';
+import 'package:af_weather/bloc/theme/theme_bloc.dart';
+import 'package:af_weather/bloc/theme/theme_state.dart';
 import 'package:af_weather/bloc/weather/weather_bloc.dart';
+import 'package:af_weather/bloc/weather/weather_events.dart';
 import 'package:af_weather/bloc/weather/weather_state.dart';
+import 'package:af_weather/models/weather.dart';
 import 'package:af_weather/repositories/WeatherRepository.dart';
 import 'package:af_weather/widgets/city_selection_widget.dart';
 import 'package:af_weather/widgets/combined_condition_temperature_widget.dart';
+import 'package:af_weather/widgets/gradient_container_widget.dart';
 import 'package:af_weather/widgets/last_updated_widget.dart';
 import 'package:af_weather/widgets/location_widget.dart';
 import 'package:flutter/cupertino.dart';
@@ -23,7 +27,6 @@ class WeatherWidget extends StatefulWidget {
 
 class _WeatherWidgetState extends State<WeatherWidget> {
   WeatherBloc _weatherBloc;
-
   Completer<void> _refreshCompleter;
 
   @override
@@ -37,21 +40,7 @@ class _WeatherWidgetState extends State<WeatherWidget> {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text('Find your weather'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () async {
-                final city = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CitySelectionWidget()),
-                );
-                if (null != city) {
-                  _weatherBloc.dispatch(FetchWeatherEvent(city: city));
-                }
-              },
-            )
-          ],
+          actions: <Widget>[weatherIcon(context, _weatherBloc)],
         ),
         body: Center(
           child: BlocBuilder(
@@ -64,37 +53,26 @@ class _WeatherWidgetState extends State<WeatherWidget> {
                 return Center(child: CircularProgressIndicator());
               }
               if (state is WeatherLoadedState) {
-                print('State is $state');
                 final weather = state.weather;
+                final themeBloc = BlocProvider.of<ThemeBloc>(context);
 
                 _refreshCompleter?.complete();
                 _refreshCompleter = Completer();
 
-                return RefreshIndicator(
-                  onRefresh: () {
-                    _weatherBloc.dispatch(
-                        RefreshWeatherEvent(city: state.weather.location));
-                    return _refreshCompleter.future;
-                  },
-                  child: ListView(
-                    children: <Widget>[
-                      Text('Loaded for: ${weather.location}'),
-                      Text('weather: ${weather}'),
-                      Padding(
-                        padding: EdgeInsets.only(top: 100),
-                        child: Center(
-                          child: LocationWidget(location: weather.location),
+                return BlocBuilder(
+                  bloc: themeBloc,
+                  builder: (_, ThemeState themeState) =>
+                      GradientContainerWidget(
+                        color: themeState.color,
+                        child: RefreshIndicator(
+                          onRefresh: () {
+                            _weatherBloc.dispatch(RefreshWeatherEvent(
+                                city: state.weather.location));
+                            return _refreshCompleter.future;
+                          },
+                          child: weatherDisplay(weather),
                         ),
                       ),
-                      Center(
-                        child: LastUpdatedWidget(dateTime: weather.lastUpdated),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.symmetric(vertical: 50.0),
-                          child: CombinedConditionTemperatureWidget(
-                              weather: weather))
-                    ],
-                  ),
                 );
               }
 
@@ -109,3 +87,33 @@ class _WeatherWidgetState extends State<WeatherWidget> {
         ),
       );
 }
+
+Widget weatherDisplay(Weather weather) => ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 100),
+          child: Center(
+            child: LocationWidget(location: weather.location),
+          ),
+        ),
+        Center(
+          child: LastUpdatedWidget(dateTime: weather.lastUpdated),
+        ),
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            child: CombinedConditionTemperatureWidget(weather: weather))
+      ],
+    );
+
+Widget weatherIcon(BuildContext ctx, WeatherBloc bloc) => IconButton(
+      icon: Icon(Icons.search),
+      onPressed: () async {
+        final city = await Navigator.push(
+          ctx,
+          MaterialPageRoute(builder: (context) => CitySelectionWidget()),
+        );
+        if (null != city) {
+          bloc.dispatch(FetchWeatherEvent(city: city));
+        }
+      },
+    );
